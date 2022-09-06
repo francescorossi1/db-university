27fa69c8ce393d8c0e3ab566346e0bd930ad72de
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-09-06 16:06:49 +0200

    fixed exercise 3 select

--- query.sql
+++ query.sql
@@ -8,7 +8,7 @@ SELECT * FROM `courses` WHERE `cfu` > 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT * FROM `students`
-WHERE `date_of_birth` <= '1991-09-06'
+WHERE TIMESTAMPDIFF(YEAR,`date_of_birth`,CURRENT_DATE()) > 30
 ORDER BY `date_of_birth` DESC;
 
 
@@ -17,10 +17,9 @@ SELECT * FROM `courses`
 WHERE `period` = 'I semestre'
 AND `year` = 1;
 
--- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
 
--- 20/06/2020 (21)
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
 
