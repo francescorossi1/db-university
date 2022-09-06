e035cf16ce2269e26312ef9c7391ebcecc48bc80
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-09-06 15:47:37 +0200

    exercise 4 select completed

--- query.sql
+++ query.sql
@@ -13,7 +13,9 @@ ORDER BY `date_of_birth` DESC;
 
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
-
+SELECT * FROM `courses`
+WHERE `period` = 'I semestre'
+AND `year` = 1;
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
 
