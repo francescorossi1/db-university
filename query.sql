6cab01e5d3222087beebe262b2e9424fe6c177c5
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-09-06 16:22:36 +0200

    exercise 7 select completed

--- query.sql
+++ query.sql
@@ -27,7 +27,7 @@ SELECT * FROM `degrees`
 WHERE `name` LIKE 'Corso di Laurea Magistrale%';
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
-
+SELECT COUNT(*) AS `numero_dipartimenti` FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
