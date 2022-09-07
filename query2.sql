1a48f6b01094964ac3de2e49c753b83c02ec8c8e
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-09-07 13:57:29 +0200

    exercise 3 join completed

--- query2.sql
+++ query2.sql
@@ -13,6 +13,17 @@ ON `departments`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT * FROM `courses`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `teachers`.`id`= `course_teacher`.`teacher_id`
+
+-- Ver 1
+WHERE `teachers`.`name` = 'Fulvio' 
+AND `teachers`.`surname` = 'Amato';
+-- Ver 2
+WHERE `teachers`.`id` = 44;
 
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
